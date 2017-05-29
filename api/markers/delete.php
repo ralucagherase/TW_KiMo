c6caf9e3dd77d67af13f850f594a3762703bd298
dragomiririna <irina_rock96@yahoo.com>
2017-05-29 12:18:29 +0300

    Create delete marker

--- api/markers/delete.php
+++ api/markers/delete.php
@@ -5,3 +5,11 @@
  * Date: 28.05.2017
  * Time: 16:32
  */
+
+	require_once '../../database/dbconfig.php';
+
+	$markers = $db_con->prepare("DELETE FROM tbl_markers WHERE id=:id");
+	echo $markers->execute([
+        ':id' => $_POST['id']
+    ]);
+	exit;
